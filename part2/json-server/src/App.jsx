37bf92e8705b8da3b7ad5b783051173489f5c2b3
ASTIK <astik.ytt@gmail.com>
2024-05-13 00:46:04 +0200

    Create base functionality for note retrieval (GET) and creation (POST)

--- part2/json-server/src/App.jsx
+++ part2/json-server/src/App.jsx
@@ -1,10 +1,75 @@
-function App() {
+import { useEffect, useState } from "react";
 
-  return (
-    <>
-      
-    </>
-  )
+const newNoteInitialValue = {
+    content: "",
+    important: false,
+};
+
+function App() {
+    const [notes, setNotes] = useState([]);
+    const [notesLoaded, setNotesLoaded] = useState(false);
+    const [newNote, setNewNote] = useState(newNoteInitialValue);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/notes")
+            .then(res => res.json())
+            .then(data => {
+                setNotes(data);
+                setNotesLoaded(true);
+            });
+    }, []);
+
+    function saveNewNote() {
+        if (newNote.content == "") return;
+
+        fetch("http://localhost:3000/notes", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                ...newNote,
+                id: notes.length + 1,
+            }),
+        })
+            .then(res => res.json())
+            .then(data => {
+                setNotes(notes.concat(data));
+                setNewNote(newNoteInitialValue);
+            });
+    }
+
+    return (
+        <div>
+
+            {notesLoaded
+                ?
+                <ul>
+                    {notes.map(note =>
+                        <li key={note.id}>
+                            {note.id} - {note.content}
+                        </li>
+                    )}
+                </ul>
+                :
+                <p>Loading...</p>
+            }
+
+            
+
+            <input
+                type="text"
+                value={newNote.content}
+                onChange={e => setNewNote({...newNote, content: e.target.value})}
+            />
+            <span>Important:</span>
+            <input
+                type="checkbox"
+                checked={newNote.important}
+                onChange={e => setNewNote({...newNote, important: e.target.checked})}
+            />
+            <button onClick={saveNewNote}>Save</button>
+
+        </div>
+    )
 }
 
 export default App
