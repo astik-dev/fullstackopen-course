255fbd447c2d62b3965746d5a0d8e5e8ea6081b8
ASTIK <astik.ytt@gmail.com>
2024-05-18 14:37:59 +0200

    part3/express-1: Integrate Mongoose for MongoDB connection

--- part3/express-1/index.js
+++ part3/express-1/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors');
+const Note = require('./models/note');
+
 
 const app = express();
 
@@ -22,56 +25,31 @@ morgan.token("body", request => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes);
+    });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(note => note.id == id);
-
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            response.status(400).send({ error: 'malformatted id' });
+        });
 });
-
-app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter(note => note.id != id);
-
-    response.status(204).end();
-});
-
-function generateId() {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0;
-    return maxId + 1;
-}
 
 app.post("/api/notes", (request, response) => {
     
@@ -83,15 +61,14 @@ app.post("/api/notes", (request, response) => {
         });
     } 
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId()
-    }
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -100,7 +77,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
