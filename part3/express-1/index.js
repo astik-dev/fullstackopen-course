39f1fe5121380179112dcb1ce36630a32bf9f5d1
ASTIK <astik.ytt@gmail.com>
2024-05-18 15:03:30 +0200

    part3/express-1: Add errorHandler middleware and support for DELETE and PUT requests

--- part3/express-1/index.js
+++ part3/express-1/index.js
@@ -36,7 +36,7 @@ app.get("/api/notes", (request, response) => {
     });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -45,10 +45,31 @@ app.get("/api/notes/:id", (request, response) => {
                 response.status(404).end();
             }
         })
-        .catch(error => {
-            console.log(error);
-            response.status(400).send({ error: 'malformatted id' });
-        });
+        .catch(error => next(error));
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+    
+    const {body} = request;
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+    
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -76,6 +97,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+
+    if (error.name == "CastError") {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
