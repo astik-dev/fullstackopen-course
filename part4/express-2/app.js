6861b01352d54a80e3999b6c928298c786be96dd
ASTIK <astik.ytt@gmail.com>
2024-05-19 15:51:40 +0200

    part4/express-2: Add users controller, tests, and User schema

--- part4/express-2/app.js
+++ part4/express-2/app.js
@@ -4,6 +4,7 @@ require("express-async-errors");
 const morgan = require("morgan");
 const cors = require("cors");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require('./controllers/users');
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -32,7 +33,8 @@ if (process.env.NODE_ENV !== 'test') {
     app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 }
 
-app.use('/api/notes', notesRouter)
+app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
